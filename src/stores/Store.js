d4804e9ed4a5d72bd42473809dd30d2bbf119189
ed phillips <ed@cronos.net>
2016-07-26 18:01:08 -0700

    data loading

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -4,8 +4,8 @@ import  { observable, computed } from 'mobx';
 
 export default class Store {
     //Autorun is a function from MobX that runs everytime that something inside is
-    name = "blah";
-    @observable numClicks = 0;
+   // name = "blah";
+   // @observable numClicks = 0;
 
     //  @computed get oddOrEven() {
     //      return this.numClicks % 2 === 0 ? 'even' : 'odd'
@@ -14,7 +14,7 @@ export default class Store {
     //    var data = d3.csvParse(string);
     //psv = d3.dsvFormat("|");
 
-    //muumble = psv.parse("foo|bar\n1|2");
+//    muumble = psv.parse("foo|bar\n1|2");
 
     @observable experiments =  d3.csv.parse("morley.csv", function(error, experiments) {
         experiments.forEach(function(x) {
@@ -22,4 +22,5 @@ export default class Store {
         });
     });
 
+                                          // ndx = crossfilter(experiments);
 }
