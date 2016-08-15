91c99f22e19217a4558cf0871afb5e07ece57de8
jody edmondson <thisjody@gmail.com>
2016-08-15 12:28:24 -0700

    review time format again

--- src/components/charts/ThrashDashDC.js
+++ src/components/charts/ThrashDashDC.js
@@ -15,11 +15,13 @@ export default class BeerDashDC {
     render() {
         console.log('in render');
         d3.json('src/stores/thrashtown.json', function (error, data) {
-            //var surfData = data;
+            var surfData = data;
 
-            var date = d3.time.format("%Y-%m-%d").parse("2016-11-28");
+            //var date = d3.time.format("%Y-%m-%d").parse("2016-11-28");
             //var date = d3.time.format("%Y-%m-%d").parse("2016-11-28T00:51:00.000z");
-            console.log(date);
+            var date = new Date("2016-01-28T00:51:00.000z");
+            //console.log(date.getUTCMonth());
+            //console.log(date.getUTCFullYear());
             //var fullDateFormat = d3.time.format('%a, %d %b %Y %X %Z');
             //var fullDateFormat = d3.time.format('%a, %d %b %Y %X %Z');
             //var yearFormat = d3.time.format('%Y');
@@ -29,8 +31,24 @@ export default class BeerDashDC {
 
             //console.log(surfData);
             surfData.forEach(d=>{
-                //d.sessionDateDt = fullDateFormat.parse(d.sessionDate);
-                //console.log(d.sessionDateDt);
+                /*d.sessionDateDt = new Date(d.sessionDate);
+                d.sessionYearDt = d.sessionDateDt.getUTCFullYear();
+                d.sessionMonthDt = d.sessionDateDt.getUTCMonth();
+                console.log(d.sessionDateDt);
+                console.log(d.sessionYearDt);
+                console.log(typeof(d.sessionMonthDt));*/
+
+                const fullDateFormat = d3.time.format("%a, %d %b %Y %X %Z");
+                const yearFormat = d3.time.format('%Y');
+                const monthFormat = d3.time.format('%b');
+                const dayOfWeekFormat = d3.time.format('%a');
+
+                d.sessionDateFormatted = fullDateFormat(new Date(d.sessionDate));
+                d.sessionYear = yearFormat(new Date(d.sessionDate));
+                //d.sessionYear = yearFormat(d.sessionDateFormatted);
+                console.log(d.sessionDateFormatted);
+                console.log(d.sessionYear);
+
             });
 
             var ttx = crossfilter(surfData);
