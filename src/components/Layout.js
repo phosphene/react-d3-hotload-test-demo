e2e561f565bf3bc6922a267d89aa652019c27120
jody edmondson <thisjody@gmail.com>
2016-08-16 13:05:05 -0700

    Thrash Dash

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Layout({ children }) {
   return (
     <div>
-      <h1>Beer Example</h1>
+      <h1>Thrash Dash</h1>
       {children}
     </div>
   )
