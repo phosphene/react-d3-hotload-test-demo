d4bca8a655e26ee39f4bd19ae71df2de9eb55cc2
jody edmondson <thisjody@gmail.com>
2016-08-15 21:06:40 -0700

    now redering charts

--- src/components/charts/ThrashDashDC.js
+++ src/components/charts/ThrashDashDC.js
@@ -21,26 +21,77 @@ export default class BeerDashDC {
             const fullDateFormat = d3.time.format("%a, %d %b %Y %X %Z");
             const yearFormat = d3.time.format('%Y');
             const monthFormat = d3.time.format('%b');
-            const dayOfWeekFormat = d3.time.format('%a');
+            const dayFormat = d3.time.format('%a');
 
             surfData.forEach(d=>{
               /*d.sessionDateDt = new Date(d.sessionDate);
                  d.sessionYearDt = d.sessionDateDt.getUTCFullYear();
                  d.sessionMonthDt = d.sessionDateDt.getUTCMonth();
-                 console.log(d.sessionDateDt);
-                 console.log(d.sessionYearDt);
                  console.log(typeof(d.sessionMonthDt));*/
 
-                const dateObj  = new Date(d.sessionDate);
+                let dateObj  = new Date(d.sessionDate);
                 d.sessionDateFormatted = fullDateFormat(dateObj);
-                d.sessionYear = yearFormat(dateObj);
-                console.log(d.sessionDateFormatted);
+                d.sessionYear = +yearFormat(dateObj);
                 console.log(d.sessionYear);
+                d.sessionMonth = monthFormat(dateObj);
+                d.sessionDay = dayFormat(dateObj);
 
             });
 
-            var ttx = crossfilter(surfData);
-            //console.log(ttx);
+            const ttx = crossfilter(surfData);
+
+            // create dimensions (x-axis values)
+            //var yearDim  = ttx.dimension(function(d) {return d.sessionYear;}),
+            var yearDim  = ttx.dimension(pluck("sessionYear"));
+
+            // create groups (y-axis values)
+            var all = ttx.groupAll();
+            var countPerYear = yearDim.group().reduceCount();
+
+            const yearChart = pieChart('#chart-ring-year');
+                //monthChart = pieChart('#chart-ring-month'),
+                //dayChart = pieChart('#chart-ring-day');
+
+            yearChart
+              .width(150)
+              .height(150)
+              .dimension(yearDim)
+              .group(countPerYear)
+              .innerRadius(20);
+
+            /*monthchart
+              .width(150)
+              .height(150)
+              .dimension(monthdim)
+              .group(countpermonth)
+              .innerradius(20)
+              .ordering(function (d) {
+                var order = {
+                  'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4,
+                  'may': 5, 'jun': 6, 'jul': 7, 'aug': 8,
+                  'sep': 9, 'oct': 10, 'nov': 11, 'dec': 12
+                };
+                return order[d.key];
+              });
+
+            daychart
+              .width(150)
+              .height(150)
+              .dimension(dayofweekdim)
+              .group(countperday)
+              .innerradius(20)
+              .ordering(function (d) {
+                var order = {
+                  'mon': 0, 'tue': 1, 'wed': 2, 'thu': 3,
+                  'fri': 4, 'sat': 5, 'sun': 6
+                }
+                return order[d.key];
+              }
+             );*/
+
+            //draw the viz!
+            renderAll();
+
         });
     }
 }
