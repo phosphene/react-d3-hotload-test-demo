8b33c777da0b228be6f559bca8bb92915d2f503d
ed phillips <ed@cronos.net>
2016-08-22 07:45:49 -0700

    add style to Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function Layout({ children }) {
+  const h1Style = { textAlign: "center",
+                    border: "5px"};
   return (
     <div>
-      <h1>Thrash Dash</h1>
+      <h1 style={h1Style}>Thrash Dash</h1>
       {children}
     </div>
   )
