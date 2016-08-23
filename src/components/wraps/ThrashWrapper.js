d37b7d264e063a5ae546e710263543af4699bca8
jody edmondson <thisjody@gmail.com>
2016-08-23 15:07:14 -0700

    reforming html

--- src/components/wraps/ThrashWrapper.js
+++ src/components/wraps/ThrashWrapper.js
@@ -10,96 +10,55 @@ class ThrashWrapper extends React.Component {
 
     }
 
-
-
- /**
-
-We must convert regular html to jsx
-here is what the regular html was
-
-       <div id="us-chart">
-       <strong>VC Distribution by States (color: total amount raised)</strong>
-       <a class="reset" href="javascript:usChart.filterAll();dc.redrawAll();" style="display: none;">reset</a>
-       <span class="reset" style="display: none;"> | Current filter: <span class="filter"></span></span>
-       <div class="clearfix"></div>
-       </div>
-**/
-
-//and below in the render is the jsx
-
-//note the special case of the onclick for the resets
-// the html was like this        <a class="reset" href="javascript:usChart.filterAll();dc.redrawAll();" style="display: none;">reset</a>
-// note also that style is absent from the jsx, we can fix that as well
-// the reset minus the style looks like this
-//              <a className="reset" onClick={clickUSReset} href="#" >reset</a>
-// note the curly braces and the constant clickUSReset which is a function and the function
-//is defined in the dashboard class which calls usChart.filterall() and redrawall()
-//here are the other resets
-// <a class="reset" href="javascript:industryChart.filterAll();dc.redrawAll();" style="display: none;">reset</a>
-//        <a class="reset" href="javascript:roundChart.filterAll();dc.redrawAll();" style="display: none;">reset</a>
-//        <a href="javascript:dc.filterAll(); dc.renderAll();">Reset All</a>
     render() {
 
-        //style="display: none;"
-        // lets try a style tag the react way ;-)
-        //based on this example
-        /**
+        const pStyle = { marginRight: "15px"};
+        const cursorStyle = { display: 'none', cursor: 'pointer'};
 
-            var divStyle = {
-            color: 'white',
-            backgroundImage: 'url(' + imgUrl + ')',
-            WebkitTransition: 'all', // note the capital 'W' here
-            msTransition: 'all' // 'ms' is the only lowercase vendor prefix
-            };
-        **/
-        const aStyle = { display: 'none'};
+        const clickReset = (x) => {
+          console.log(x);
+        }
 
         return (
 
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-xs-12 dc-data-count dc-chart" id="data-count">
-                      <h2>Session History
-                        <small>
-
-                           <a id="all" href="#">Reset All</a>
-
-                        </small>
-                      </h2>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-xs-2">
+                    <div id="chart-ring-year">
+                        <strong>Year</strong>
+                        <a className="reset" onClick={()=>clickReset("chart-ring-year")} style={cursorStyle}> reset</a>
+                        <div className="clearfix"></div>
                     </div>
-                  </div>
-                  <div className="row" id="control-row">
-                    <div className="col-xs-2 pie-chart">
-                      <h4>Year <small><a id="year">reset</a></small></h4>
-                      <div className="dc-chart" id="chart-ring-year"></div>
-                    </div>
-                    <div className="col-xs-2 pie-chart">
-                      <h4>Month <small><a id="month" href="#">reset</a></small></h4>
-                      <div className="dc-chart" id="chart-ring-month"></div>
-                    </div>
-                    <div className="col-xs-2 pie-chart">
-                      <h4>Day <small><a id="day">reset</a></small></h4>
-                      <div id="chart-ring-day" className="dc-chart"></div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-xs-6 col-md-3">
-                      <div className="dc-chart" id="chart-bar-fun-factor"></div>
-                    </div>
-                    <div className="col-xs-6 col-md-3">
-                      <div className="dc-chart" id="chart-bar-crowd-factor"></div>
-                    </div>
-                    <div className="col-xs-6 col-md-3">
-                      <div className="dc-chart" id="chart-bar-hollow-factor"></div>
-                    </div>
-                    <div className="col-xs-6 col-md-3">
-                      <div className="dc-chart" id="chart-bar-quality-factor"></div>
-                    </div>
-                  </div>
                 </div>
-
-
-                        );
+                {/*<div className="col-xs-2 pie-chart">
+                  <h4>Year <small><a id="year">reset</a></small></h4>
+                  <div className="dc-chart" id="chart-ring-years"></div>
+                </div>*/}
+                <div className="col-xs-2 pie-chart">
+                  <h4>Month <small><a id="month" href="#">reset</a></small></h4>
+                  <div className="dc-chart" id="chart-ring-month"></div>
+                </div>
+                <div className="col-xs-2 pie-chart">
+                  <h4>Day <small><a id="day">reset</a></small></h4>
+                  <div id="chart-ring-day" className="dc-chart"></div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-xs-6 col-md-3">
+                  <div className="dc-chart" id="chart-bar-fun-factor"></div>
+                </div>
+                <div className="col-xs-6 col-md-3">
+                  <div className="dc-chart" id="chart-bar-crowd-factor"></div>
+                </div>
+                <div className="col-xs-6 col-md-3">
+                  <div className="dc-chart" id="chart-bar-hollow-factor"></div>
+                </div>
+                <div className="col-xs-6 col-md-3">
+                  <div className="dc-chart" id="chart-bar-quality-factor"></div>
+                </div>
+              </div>
+            </div>
+        );
 
     }
 
