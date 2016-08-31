40c6dad1e7ef2b4115c223f612bc1195d85198cd
jody edmondson <thisjody@gmail.com>
2016-08-31 13:00:29 -0700

    switched to Nas to deconstruct charts

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 import Layout from './Layout';
 import ThrashWrapper from "./wraps/ThrashWrapper";
+import NasDashWrapper from "./wraps/NasDashWrapper";
 class App extends Component {
   render() {
     return (
       <Layout>
-        <ThrashWrapper/>
+      {/*<ThrashWrapper/>*/}
+        <NasDashWrapper/>
       </Layout>
     );
   }
